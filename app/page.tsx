ae2af92aa57b7e6623a4fccf404bb649eba8cfa2
satyamneelkamal <crickaryt@gmail.com>
2025-08-20 22:06:46 +0530

    Landing Page Completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@
   import IntegrationsSection from '../components/integrations'
   import StatsSection from '../components/stats'
   import PricingSection from '../components/pricing'
+  import CommunitySection from '../components/community'
+  import WaitList from '../components/waitlist'
+  import FaqSection from '../components/faqs'
+  import FooterSection from '@/components/footer'
+
 
   export default function Home() {
     return (
@@ -12,6 +17,10 @@
         <IntegrationsSection />
         <StatsSection />
         <PricingSection />
+        <CommunitySection />
+        <WaitList />
+        <FaqSection />
+        <FooterSection />
       </>
     )
   }
